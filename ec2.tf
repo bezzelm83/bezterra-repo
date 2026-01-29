0def38ee364fb6a0f321d2fa23c595768c18da9c
bezzelm83 <bezzelm@gmail.com>
2026-01-29 11:37:23 +0200

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,23 @@
+# Configure the Microsoft Azure Provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
 }
+
+# Create a resource group
 resource "azurerm_resource_group" "example" {
-  name     = "example"
-  location = "West Europe"
+  name     = "testResourceGroup"
+  location = "West US"
+
+  tags = {
+    environment = "Production"
+  }
 }
